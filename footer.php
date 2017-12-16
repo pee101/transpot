c4990bbb11b3e8aaf9930378d2fd1f5e55ba4a0a
Pirachai chaijinda <mou11262@gmail.com>
2017-12-16 16:26:04 +0700

    fix footer

--- footer.php
+++ footer.php
@@ -12,7 +12,9 @@
 <br>
 <br>
 <br>
-
+<br>
+<br>
+    
     <hr>
     <footer class="row">
         <p class="col-md-9 col-sm-9 col-xs-12 copyright">&copy; <a href="http://usman.it" target="_blank">Muhammad
